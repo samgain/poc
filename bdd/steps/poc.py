22716b68c3ffdc58b8b52080d46b839ca20712b2
samga <sg@samga.in>
2018-02-03 23:49:20 +0000

    .

--- bdd/steps/poc.py
+++ bdd/steps/poc.py
@@ -6,7 +6,7 @@ def step_impl(context):
 
 @when('we implement a test')
 def step_impl(context):
-    assert True is False
+    assert True is not False
 
 @then('behave will test it for us!')
 def step_impl(context):
